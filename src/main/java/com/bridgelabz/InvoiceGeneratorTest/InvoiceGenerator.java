242f7f02df1ab3d3ab35ca20bb56a84ab4e2d879
HarishKamaras <harishkamarasu@gmail.com>
2022-06-06 18:32:17 +0530

    InvoiceGenerator

---
+++ src/main/java/com/bridgelabz/InvoiceGeneratorTest/InvoiceGenerator.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.InvoiceGeneratorTest;
+
+public class InvoiceGenerator {
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUMFARE = 5;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare= distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if(totalFare< MINIMUMFARE){
+            return MINIMUMFARE;
+        }
+        return totalFare;
+    }
+}
---
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest/InvoiceGeneratorTest.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.InvoiceGeneratorTest;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceGeneratorTest {
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnFare(){
+        InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
+        double distance=2.0;
+        int time=5;
+        double fare=invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals("fare",25,fare,0.0);
+
+    }
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare(){
+        InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
+        double distance=0.1;
+        int time=1;
+        double fare=invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals("fare",5,fare,0.0);
+    }
+}
